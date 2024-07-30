b572071c18b6f66ea3ec8235a9cead3c1acaafb7
zhao-zg <61549925+zhao-zg@users.noreply.github.com>
2024-07-30 11:05:52 +0800

    Update api.py

--- api.py
+++ api.py
@@ -82,7 +82,7 @@ async def loginPhone():
 async def login():
     print("login")
     data = await request.get_json()
-    data.set("type", "password")
+    data["type"] = "password"
     return loginPublic(data)
     
 # 传入账号密码，启动登录线程
