61f4b76ea7cf13b260d52c5eda76954b7e268fa6
zhao-zg <61549925+zhao-zg@users.noreply.github.com>
2024-12-10 10:43:21 +0800

    Update login.py

--- login.py
+++ login.py
@@ -18,6 +18,7 @@ import base64
 import io
 import re
 import logging
+from fake_useragent import UserAgent
 
 # 传参获得已初始化的ddddocr实例
 ocr = None
@@ -27,6 +28,14 @@ logger = logging.getLogger("login")
 simple_format = "[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d] %(message)s"
 logging.basicConfig(level=logging.INFO, format=simple_format, datefmt="%Y-%m-%d %H:%M:%S %z")
 
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+]
 # 支持的形状类型
 supported_types = [
     "三角形",
@@ -119,7 +128,7 @@ async def loginPhone(chromium_path, workList, uid, headless):
     )
     page = await browser.newPage()
     await page.setUserAgent(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        UserAgent().random
     )
     await page.setViewport({"width": 360, "height": 640})
     await page.goto(
@@ -298,7 +307,7 @@ async def loginPassword(chromium_path, workList, uid, headless):
     )
     page = await browser.newPage()
     await page.setUserAgent(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        UserAgent().random
     )
     await page.setViewport({"width": 360, "height": 640})
     await page.goto(
@@ -1182,6 +1191,8 @@ async def main(workList, uid, oocr, oocrDet):
     logger.info("初始化浏览器。。。。。")
     chromium_path = await init_chrome()
     headless = 'new'
+    if platform.system() == "Windows":
+        headless = False
     logger.info("进入选择登录方式流程")
 
     try_time = 1
